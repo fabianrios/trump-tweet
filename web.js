a49bb3de9bbf78d195b1ca5e3b6a0331149eb634
fabian rios <fabian.rios@metals-hub.com>
2019-05-08 15:32:35 +0200

    update with no classes

--- web.js
+++ web.js
@@ -1,21 +1,25 @@
-
 var fa = document.createElement('style');
     fa.type = 'text/css';
     fa.textContent = '@font-face { font-family: i eat crayons; src: url("'
         + chrome.extension.getURL('fonts/i eat crayons.ttf')
         + '"); }';
 document.head.appendChild(fa);
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 function changeFont(){
-	var content = document.getElementsByClassName('content');
-	for(var i=0; i<content.length;i++){
-		var c = content[i].childNodes[0].nextElementSibling;
-		var username =  c.querySelectorAll('.account-group .username');
-		if(!!username && username[0].innerText == '@realDonaldTrump' ){
-			var tweets = content[i].querySelectorAll('.js-tweet-text-container .TweetTextSize');
-			for(var j =0; j < tweets.length;j++){
-				tweets[j].style.fontFamily = "'i eat crayons', sans-serif";
-				tweets[j].style.color = 'darkred';
-				tweets[j].style.fontSize = '24px';
+  const tl = document.getElementsByTagName('article')
+	for (var tweet of tl) {
+		for (var items of tweet.children) {
+			const firstone = items.getElementsByTagName('a')[0]
+			if (firstone && firstone.hasAttribute('href') && items.hasAttribute('data-testid') && firstone.getAttribute('href') === '/IvanDuque') {
+        for (var span of items.getElementsByTagName('span')) {
+          span.style.fontFamily = "'i eat crayons', sans-serif";
+          span.style.color = 'orange';
+          span.style.fontSize = '20px';
+        }
 			}
 		}
 	}
